5c17d502b80021e3d3af35a39939604170d4b1a0
Thomas Rudin <thomas@rudin.io>
2018-07-27 08:30:26 +0200

    fa

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,69 @@
 (function(){
 
 	var state = {
-		score: []
+		score: [],
+		start: 0
 	};
+
+	function update(){
+		m.request("api/highscore?start=" + state.start)
+		.then(function(score){
+			state.score = score;
+		});
+	}
+
+	function next(){
+		state.start += 10;
+		update();
+	}
+
+	function previous(){
+		state.start = Math.max(state.start - 10, 0);
+		update();
+	}
+
+	var NextButton = {
+		view: function(){
+			return m("div", {class:"btn-group"}, [
+				m("button", {class:"btn btn-secondary btn-lg", onclick: previous}, [
+					m("i", {class:"fas fa-step-backward"}),
+					" Previous"
+				]),
+				m("button", {class:"btn btn-secondary btn-lg", onclick: next}, [
+					"Next ",
+					m("i", {class:"fas fa-step-forward"})
+				])
+			]);
+		}
+	}
 
 	var HighscoreRows = {
 		view: function(){
 			var rows = [];
-			var ranking = 1;
+			var ranking = state.start + 1;
 
 			state.score.forEach(function(player){
 
 				var time_diff = moment(moment()).diff(player.modified);
 				var last_login = moment.duration(time_diff).humanize();
 				var is_online = time_diff < 30000;
+				var is_inactive = time_diff > (1000*3600*24*30); //30 days
 
 				var state = m("span", {class: "badge badge-" + (is_online?"success":"danger")}, is_online?"Online":"Offline");
+				var xp = m("span", {class: "badge badge-info"}, player.xp);
+				var inactive = m("span", {class: "badge badge-secondary"}, "Inactive")
 
 				rows.push(m("tr", [
 					m("td", ranking++),
 					m("td", player.name),
-					m("td", player.xp),
+					m("td", xp),
 					m("td", player.attributes.digged_nodes),
 					m("td", player.attributes.crafted),
 					m("td", player.attributes.placed_nodes),
 					m("td", player.attributes.died),
 					m("td", player.attributes.played_time ? moment.duration(+player.attributes.played_time, "seconds").humanize() : ""),
-					m("td", last_login),
-					state
+					m("td", [last_login, (is_inactive?inactive:null)]),
+					m("td", state)
 				]));
 
 			});
@@ -58,14 +94,13 @@
 		}
 	};
 
-	m.mount(document.getElementById("app"), Table);
-
-	function update(){
-		m.request("api/highscore")
-		.then(function(score){
-			state.score = score;
-		});
+	var Page = {
+		view: function(){
+			return m("div", [m(NextButton), m(Table)]);
+		}
 	}
+
+	m.mount(document.getElementById("app"), Page);
 
 	update();
 
--- src/api/highscore.js
+++ src/api/highscore.js
@@ -5,7 +5,17 @@ const pool = require('../pool');
 app.get('/api/highscore', function (req, res) {
 	const params = req.body;
 
-	var query = "select p.name, p.modification_date, p.creation_date, md.value from player p join player_metadata md on p.name = md.player where md.attr = 'xp' order by md.value::int desc limit 20"
+	var offset = 0;
+
+	if (req.query && req.query.start) {
+		var o = parseInt(req.query.start);
+		if (o && o > 0)
+			offset = o;
+	}
+
+	var query = "select p.name, p.modification_date, p.creation_date, md.value from player p";
+	query += " join player_metadata md on p.name = md.player";
+	query += " where md.attr = 'xp' order by md.value::int desc limit 10 offset " + offset;
 
 	pool
 	.query(query)
@@ -21,6 +31,9 @@ app.get('/api/highscore', function (req, res) {
 		));
 
 		var promises = result.rows.map(row => {
+			//sethome:home
+			//"homedecor:player_skin"
+			//const FIELDS = ["crafted", "placed_nodes", "died", "digged_nodes", "xp", "played_time"]
 			return pool.query("select attr, value from player_metadata where player = $1", [row.name])
 			.then(result => {
 				var res = {};
@@ -40,7 +53,7 @@ app.get('/api/highscore', function (req, res) {
 	})
 	.then(result => res.json(result))
 	.catch(e => {
-		consoler.error(e);
+		console.error(e);
 		res.status(500).end();
 	});
 
