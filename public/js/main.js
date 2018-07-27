ccbf9df350cc1fd8093e37e398ea019c66049308
Thomas Rudin <thomas@rudin.io>
2018-07-27 08:50:04 +0200

    xp ranks

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,34 @@
 
 	var state = {
 		score: [],
-		start: 0
+		start: 0,
+		ranks: []
 	};
+
+	function numberWithCommas(x){
+		if (x == undefined)
+			return "";
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+	}
+
+	//get ranks (already ordered ascending)
+	m.request("js/ranks.json")
+	.then(function(ranks){ state.ranks = ranks });
+
+	function getRank(xp){
+		var currentRank = null;
+
+		for (var i=0; i<state.ranks.length; i++){
+			var rank = state.ranks[i];
+
+			if (rank.xp > xp)
+				break;
+
+			currentRank = rank;
+		}
+
+		return currentRank;
+	}
 
 	function update(){
 		m.request("api/highscore?start=" + state.start)
@@ -50,19 +76,33 @@
 				var is_inactive = time_diff > (1000*3600*24*30); //30 days
 
 				var state = m("span", {class: "badge badge-" + (is_online?"success":"danger")}, is_online?"Online":"Offline");
-				var xp = m("span", {class: "badge badge-info"}, player.xp);
+				var xp = m("span", {class: "badge badge-info"}, numberWithCommas(player.xp));
 				var inactive = m("span", {class: "badge badge-secondary"}, "Inactive")
 
+				var rank = getRank(player.xp);
+
+				var rankCol = m("span");
+
+				if (rank){
+					var color = rank.color;
+					rankCol = m("span", [
+						m("img", {src:"img/xp/" + rank.icon}),
+						" ",
+						m("span", {"style":"color: rgb("+color.r+","+color.g+","+color.b+")"}, rank.name)
+					]);
+				}
+
 				rows.push(m("tr", [
-					m("td", ranking++),
+					m("td", m("h1", {class:"badge badge-primary"}, "" + ranking++)),
 					m("td", player.name),
 					m("td", xp),
-					m("td", player.attributes.digged_nodes),
-					m("td", player.attributes.crafted),
-					m("td", player.attributes.placed_nodes),
+					m("td", rankCol),
+					m("td", numberWithCommas(player.attributes.digged_nodes)),
+					m("td", numberWithCommas(player.attributes.crafted)),
+					m("td", numberWithCommas(player.attributes.placed_nodes)),
 					m("td", player.attributes.died),
 					m("td", player.attributes.played_time ? moment.duration(+player.attributes.played_time, "seconds").humanize() : ""),
-					m("td", [last_login, (is_inactive?inactive:null)]),
+					m("td", [last_login, " ", (is_inactive?inactive:null)]),
 					m("td", state)
 				]));
 
@@ -80,6 +120,7 @@
 						m("th", "Ranking"),
 						m("th", "Name"),
 						m("th", "XP"),
+						m("th", "Rank"),
 						m("th", "Dig-count"),
 						m("th", "Craft-count"),
 						m("th", "Build-count"),
